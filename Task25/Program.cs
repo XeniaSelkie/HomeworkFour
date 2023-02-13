9d6eba5bb0b105ead26958062e1c1d539d57367f
Sana <mypostoffice94@gmail.com>
2023-02-13 16:52:19 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Task25();
+
+void Task25()
+{
+    int A = ReadInt("первое число");
+    int B = ReadInt("второе число");
+    
+    Exponentiation(A, B);
+}
+
+int ReadInt(string argument)
+{
+	Console.Write($"Введите {argument}: ");
+    int number;
+
+	while (!int.TryParse(Console.ReadLine(), out number))
+	{
+		Console.WriteLine("Ошибка ввода, пожалуйста, введите число");
+	}
+
+	return number;
+}
+
+void Exponentiation(int number, int exponent)
+{
+    int result = number;
+    for (int count = 1; count < exponent; count++)
+    result = result * number;     
+    Console.WriteLine($"Число {number}, возведённое в степень {exponent} равняется {result}");
+}

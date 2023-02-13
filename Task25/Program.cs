ce9097d6c2ee378cc1b40a2c4d3e10112b379f5d
Sana <mypostoffice94@gmail.com>
2023-02-13 17:56:49 +0300

    Add Task27 and correct Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Task25();
+int A = ReadInt("первое число");
+int B = ReadInt("второе число");
+if (A != 1 && A != 0)
+Exponentiation(A, B);
+else
+Console.WriteLine($"Число {A}, возведённое в любую степень всегда равняется {A}");
 
-void Task25()
-{
-    int A = ReadInt("первое число");
-    int B = ReadInt("второе число");
-    
-    Exponentiation(A, B);
-}
+
 
 int ReadInt(string argument)
 {
@@ -20,13 +19,19 @@ int ReadInt(string argument)
 		Console.WriteLine("Ошибка ввода, пожалуйста, введите число");
 	}
 
-	return number;
+    return number;
 }
 
 void Exponentiation(int number, int exponent)
 {
+    if (exponent == 0)
+    Console.WriteLine($"Любое число, возведённое в степень {exponent} всегда равняется 1");
+    else
+    {
     int result = number;
     for (int count = 1; count < exponent; count++)
-    result = result * number;     
+    result *= number;     
     Console.WriteLine($"Число {number}, возведённое в степень {exponent} равняется {result}");
+    }
+    
 }
---
+++ Task27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Task27();
+
+void Task27()
+{
+    int inputNumber = ReadInt("число");
+    Console.WriteLine($"Сумма цифр в числе {inputNumber} равняется {SumDigits(inputNumber)}");
+}
+
+int ReadInt(string argument)
+{
+	Console.Write($"Введите {argument}: ");
+    int number;
+
+	while (!int.TryParse(Console.ReadLine(), out number))
+	{
+		Console.WriteLine("Ошибка ввода, пожалуйста, введите число");
+	}
+
+	return number;
+}
+
+int SumDigits(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+    result += number % 10;
+    number = number / 10;
+    }
+    return result;
+}
